092a3f22dd2b8627cad9f39261f5cc17d9867546
Jos Koetsier <jos.koetsier@digital.cabinet-office.gov.uk>
2017-11-16 14:01:35 +0000

    Add taggable items to global stats

--- lib/services.rb
+++ lib/services.rb
@@ -2,4 +2,8 @@ module Services
   def self.statsd
     @statsd ||= Statsd.new
   end
+
+  def self.rummager
+    GdsApi::Rummager.new('https://www.gov.uk/api')
+  end
 end
--- spec/lib/global_stats_spec.rb
+++ spec/lib/global_stats_spec.rb
@@ -7,17 +7,31 @@ RSpec.describe GlobalStats do
 
   describe '#run' do
     it "reports statistics" do
-      stub_request(:get, "https://www.gov.uk/api/search.json?count=0&debug=include_withdrawn").
-        to_return(body: JSON.dump(total: 123))
 
-      stub_request(:get, "https://www.gov.uk/api/search.json?count=0&debug=include_withdrawn&filter_taxons=_MISSING").
+      stub_request(:get, "https://www.gov.uk/api/search.json").
+        with(query: { count: 0,
+                      debug: 'include_withdrawn' }).
+        to_return(body: JSON.dump(total: 1000))
+
+      stub_request(:get, "https://www.gov.uk/api/search.json").
+        with(query: { count: 0,
+                      debug: 'include_withdrawn',
+                      reject_content_store_document_type: GlobalStats::BLACKLIST_DOCUMENT_TYPES }).
+        to_return(body: JSON.dump(total: 500))
+
+      stub_request(:get, "https://www.gov.uk/api/search.json").
+        with(query: { count: 0,
+                      debug: 'include_withdrawn',
+                      filter_taxons: '_MISSING',
+                      reject_content_store_document_type: GlobalStats::BLACKLIST_DOCUMENT_TYPES }).
         to_return(body: JSON.dump(total: 100))
 
       GlobalStats.new.run
 
-      expect(Services.statsd).to have_received(:gauge).with("govuk.tagging.items", 123)
+      expect(Services.statsd).to have_received(:gauge).with("govuk.tagging.items", 1000)
       expect(Services.statsd).to have_received(:gauge).with("govuk.tagging.items_without_taxons", 100)
-      expect(Services.statsd).to have_received(:gauge).with("govuk.tagging.items_with_taxons", 23)
+      expect(Services.statsd).to have_received(:gauge).with("govuk.tagging.taggable_items", 500)
+      expect(Services.statsd).to have_received(:gauge).with("govuk.tagging.items_with_taxons", 400)
     end
   end
 end
